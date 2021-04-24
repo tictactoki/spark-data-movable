71c10cf8adbcd1dd234434c3f9d3f0c8b2172cc0
stephanewong <stephane.wong1@gmail.com>
2021-04-25 00:05:08 +0200

    Start NamespaceConfig enumeration

--- src/main/scala/com/movable/models/ConfigModel.scala
+++ src/main/scala/com/movable/models/ConfigModel.scala
@@ -1,5 +1,13 @@
 package com.movable.models
 
+class NamespaceConfig extends Enumeration {
+  private val Movable = "movable"
+  private val Dbs = "dbs"
+  private val Files = "files"
+  val DbsNamespace = s"$Movable.$Dbs"
+  val FilesNamespace = s"$Movable.$Files"
+}
+
 trait ConfigNamespace {
   def namespace: String
 }
