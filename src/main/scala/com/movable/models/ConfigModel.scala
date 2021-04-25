40264e7948ca45655340993519a61eb900f632ff
stephanewong <stephane.wong1@gmail.com>
2021-04-25 13:34:33 +0200

    - Create FileFormat enum - Update ConfigModel file: (add DBSConfigBuilder and FileConfigBuilder class) - Add const in NamespaceConfig

--- src/main/scala/com/movable/models/ConfigModel.scala
+++ src/main/scala/com/movable/models/ConfigModel.scala
@@ -2,26 +2,60 @@ package com.movable.models
 
 import com.typesafe.config.Config
 
+import scala.util.Try
+
 object NamespaceConfig extends Enumeration {
   private val Movable = "movable"
   private val Dbs = "dbs"
   private val Files = "files"
   val DbsNamespace = s"$Movable.$Dbs"
   val FilesNamespace = s"$Movable.$Files"
+
+
+  object DBSNamespace {
+    val Driver = "driver"
+    val Host = "host"
+    val Db = "db"
+    val Port = "port"
+    val Username = "username"
+    val Pwd = "password"
+  }
+
+  object FileNamespace {
+    val InputFileFormat = "input_file_format"
+    val InputPath = "input_path"
+    val OutputPath = "output_path"
+  }
+
 }
 
 trait ConfigNamespace {
-  def namespace: String
+  val namespace: String
 }
 
 abstract class ConfigBuilder extends ConfigNamespace {
-  def getConfigField(namespace: String, field: String) = s"$namespace.$field"
+  def getConfigField(namespace: String)(field: String) = s"$namespace.$field"
+  protected lazy val curryingNamespace: String => String = (field: String) => getConfigField(namespace)(field)
 }
 
-abstract class ServerConfigBuilder(serverName: String) extends ConfigBuilder {
-  override def namespace: String = getConfigField(NamespaceConfig.DbsNamespace, serverName)
+case class DBSConfigBuilder(config: Config, serverName: String, dbs: String) extends ConfigBuilder {
+  import NamespaceConfig.DBSNamespace._
+  override val namespace: String = getConfigField(NamespaceConfig.DbsNamespace)(serverName)
+  lazy val dbsNamespace: String = super.curryingNamespace(dbs)
+  lazy val driver: String = config.getString(s"$dbsNamespace.$Driver")
+  lazy val host: String = config.getString(s"$dbsNamespace.$Host")
+  lazy val port: Try[Int] = Try(config.getInt(s"$dbsNamespace.$Port"))
+  lazy val db: String = config.getString(s"$dbsNamespace.$Db")
+  lazy val username: Try[String] = Try(config.getString(s"$dbsNamespace.$Username"))
+  lazy val pwd: Try[String] = Try(config.getString(s"$dbsNamespace.$Pwd"))
 }
 
-abstract class FileConfigBuilder(inputSource: String) extends ConfigBuilder {
-  override def namespace: String = getConfigField(NamespaceConfig.FilesNamespace, inputSource)
+case class FileConfigBuilder(config: Config,
+                                 inputDirectorySource: String) extends ConfigBuilder {
+  import NamespaceConfig.FileNamespace._
+  override val namespace: String = getConfigField(NamespaceConfig.FilesNamespace)(inputDirectorySource)
+  lazy val fileNamespace: String => String = getConfigField(namespace)
+  lazy val inputFileFormat: String = fileNamespace(InputFileFormat)
+  lazy val inputPath: String = fileNamespace(InputPath)
+  lazy val outputPath: String = fileNamespace(outputPath)
 }
---
+++ src/main/scala/com/movable/models/FileFormat.scala
@@ -0,0 +1,7 @@
+package com.movable.models
+
+object FileFormat extends Enumeration {
+  val CSV = "csv"
+  val PARQUET = "csv"
+  val JSON = "json"
+}
